24536262d3373c0f5955a698a5eebf8cbaf45ba3
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-06-15 09:27:11 -0400

    test: captured what week the earliest data is from-establish the data period range start

--- spec/services/alpha_vantage_service_spec.rb
+++ spec/services/alpha_vantage_service_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe AlphaVantageService, :vcr do
     
     it "returns a json response with top level keys: Meta Data and Weekly Adjusted Time Series" do
       expect(@ticker).to be_a(String)
-      
+
       expect(@response).to be_a(Hash)
       expect(@response.keys).to eq([:"Meta Data", :"Weekly Adjusted Time Series"])
 
@@ -21,23 +21,27 @@ RSpec.describe AlphaVantageService, :vcr do
       expect(@response[:"Weekly Adjusted Time Series"]).to be_a(Hash)
     end
 
-    it "responds with weekly breakdowns of stock values that are numerical within the key :Weekly Adjusted Time Series" do
+    it "responds with weekly stock value info that are numerical within the key :Weekly Adjusted Time Series" do
       @response[:"Weekly Adjusted Time Series"].each do |week, values|
         expect(values.keys).to eq([:"1. open", :"2. high", :"3. low", :"4. close", :"5. adjusted close", :"6. volume", :"7. dividend amount"])
         values.each do |value|
           expect(value.last).to match(/^\d+(\.\d+)?$/) #the string response matches, either, an Integer or Floats
         end
       end
+    end
 
-      # method to calculate when the earliest data is from
-      #get the end of month for the 7 yr 11 months ago
-      #this is the last key in the returned has (the earliest data)
+    it "includes data from the week 7 years and 11 months ago - so we know the period range of the data we receive" do
+      # method to calculate when the earliest data is from (ie earliest wk key in :Weekly Adjusted Time Series)
+      #which is from the end of month for the 7 yr 11 months ago
       
-      # target_date = Date.today.prev_year(7).prev_month(11)
-      # last_day_of_month = target_date.end_of_month.day
-      # formatted_date = target_date.strftime("%Y-%m")
-      # formatted_date_with_last_day = "#{formatted_date}-#{last_day_of_month}"
-      
+      target_date = Date.today.prev_year(7).prev_month(11)
+      last_day_of_month = target_date.end_of_month.day
+      formatted_date = target_date.strftime("%Y-%m")
+
+      week_7_years_11_months_ago = "#{formatted_date}-#{last_day_of_month}"
+      earliest_week = @response[:"Weekly Adjusted Time Series"].keys.last.to_s
+
+      expect(earliest_week).to eq(week_7_years_11_months_ago)
     end
   end
 end
